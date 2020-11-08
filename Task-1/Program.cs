389b26b8df7d5ca3c203e5027ec07e6b96b62b57
hellras-game <hellras.game@gmail.com>
2020-11-08 17:40:30 +0700

    Добавьте файлы проекта.

---
+++ Task-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task_1
+{
+    class Program
+        {
+            /*Выполнила Ярмолинская Анастасия
+            С помощью рефлексии выведите все свойства структуры DateTime*/
+            static void Main(string[] args)
+                {
+                Type dateTime = typeof(DateTime);
+                foreach (var property in dateTime.GetProperties())
+                {
+                    Console.WriteLine($"{property.Name} - {property.PropertyType}");
+                }
+                }
+        }
+}
